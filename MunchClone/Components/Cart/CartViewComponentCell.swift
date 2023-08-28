cffdd6b10ab43f5911ea4050af3dbdfe99f9c615
kyletaylor94 <kyletaylorrules94@gmail.com>
2023-08-28 18:15:50 +0200

    some changes on backend side

---
+++ MunchClone/Components/Cart/CartViewComponentCell.swift
@@ -0,0 +1,18 @@
+//
+//  CartViewComponentCell.swift
+//  MunchClone
+//
+//  Created by Turdesán Csaba on 28/08/2023.
+//
+
+import SwiftUI
+
+struct CartViewComponentCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CartViewComponentCell()
+}
